8f53a3b4db91759570017af841df91bbcdcf5786
CReeshma <reeshmaavinash@gmail.com>
2024-09-05 12:41:31 +0530

    Create displayNumber.java

---
+++ displayNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class displayNumber{
+
+public static void main(String[]args){
+	int n,i;
+	 Scanner sc = new Scanner (System.in);
+	 System.out.println("Enter the number : ");
+	 n=sc.nextInt();
+	 for(i=0;i<=n;i++)
+	 {
+		 System.out.println(i);
+	 }
+	 
+}
+}
