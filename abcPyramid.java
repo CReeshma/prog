22f3d28f9912045655ccfdc602b8d2c5d0c69ad3
CReeshma <reeshmaavinash@gmail.com>
2024-11-07 18:40:54 +0530

    Create abcPyramid.java

---
+++ abcPyramid.java
@@ -0,0 +1,16 @@
+
+
+public class numbeer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;i<=5;i++) {
+			for(char j='a';j<'a'+i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
